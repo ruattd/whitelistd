3a5370af8ebb8dda1b7348c3ef20712626048325
任天天 <ruattqwq@gmail.com>
2024-01-05 23:54:43 +0800

    Optimize Record options checking

--- common/src/main/java/io/github/ruattd/fc/whitelistd/MessageHelper.java
+++ common/src/main/java/io/github/ruattd/fc/whitelistd/MessageHelper.java
@@ -1,13 +1,18 @@
 package io.github.ruattd.fc.whitelistd;
 
+import lombok.NonNull;
 import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSource;
 import net.minecraft.network.chat.Component;
 
 public class MessageHelper {
-    public static void sendSystemMessage(CommandSource target, Component message) {
+    public static void sendSystemMessage(@NonNull CommandSource target, @NonNull Component message) {
         target.sendSystemMessage(Component.empty()
                 .append(Component.empty().withStyle(ChatFormatting.YELLOW).append("Whitelistd: "))
                 .append(message));
+    }
+
+    public static void sendSystemMessage(@NonNull Component message) {
+        sendSystemMessage(Whitelistd.getInstance().getServer(), message);
     }
 }
--- common/src/main/java/io/github/ruattd/fc/whitelistd/WhitelistHelper.java
+++ common/src/main/java/io/github/ruattd/fc/whitelistd/WhitelistHelper.java
@@ -1,6 +1,8 @@
 package io.github.ruattd.fc.whitelistd;
 
 import lombok.NonNull;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 
 public final class WhitelistHelper {
     /**
@@ -18,20 +20,29 @@ public final class WhitelistHelper {
             return new SearchList.QueryResult(false, player.getName());
         }
         var result = searchList.query(player);
-        if (result.exist() || (!config.isEnableRecord())) {
-            return result;
-        } else {
+        if ((!result.exist()) || config.isEnableRecord()) {
             // Record 实现
             var name = player.getName();
-            var playerRecord = new PlayerInfo(name + ".record");
+            var nameRecord = name + ".record";
+            var playerRecord = new PlayerInfo(nameRecord);
             var resultRecord = searchList.query(playerRecord);
             if (resultRecord.exist()) {
-                searchList.addItem(player);
-                searchList.removeItem(playerRecord);
-                return new SearchList.QueryResult(true, name);
-            } else {
-                return result;
+                var addState = searchList.addItem(player);
+                if (addState != SearchList.AddItemState.SUCCESSFUL) {
+                    MessageHelper.sendSystemMessage(Component.empty().withStyle(ChatFormatting.RED)
+                            .append("Record add item failed: " + addState));
+                } else {
+                    var removeState = searchList.removeItem(playerRecord);
+                    if (removeState != SearchList.RemoveItemState.SUCCESSFUL) {
+                        MessageHelper.sendSystemMessage(Component.empty().withStyle(ChatFormatting.RED)
+                                .append("Record remove item failed: " + removeState));
+                        MessageHelper.sendSystemMessage(Component.empty().withStyle(ChatFormatting.GOLD)
+                                .append("You need to remove '" + nameRecord + "' manually or it may cause unexpected problems"));
+                    }
+                    return new SearchList.QueryResult(true, name);
+                }
             }
         }
+        return result;
     }
 }
