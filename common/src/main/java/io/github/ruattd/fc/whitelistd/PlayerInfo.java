2677945b96e6d3b5c1b0ad8f33c07fb4cf2e5f38
任天天 <ruattqwq@gmail.com>
2024-01-06 00:12:48 +0800

    Fix a bug of PlayerInfo's lombok annotations

--- common/src/main/java/io/github/ruattd/fc/whitelistd/PlayerInfo.java
+++ common/src/main/java/io/github/ruattd/fc/whitelistd/PlayerInfo.java
@@ -7,8 +7,8 @@ import java.util.UUID;
 /**
  * 玩家信息，由名称和 UUID 两部分组成，其中名称一定存在，但 UUID 可能为 {@code null}
  */
-@Data @AllArgsConstructor
-public class PlayerInfo {
+@Getter @AllArgsConstructor @RequiredArgsConstructor
+public final class PlayerInfo {
     private @NonNull String name;
     private UUID uuid;
 }
